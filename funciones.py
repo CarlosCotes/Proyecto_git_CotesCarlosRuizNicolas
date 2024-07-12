d55df9d44773d2bda99b83a2cc4e192fa04a334d
CarlosCotes <kmayo2013@gmail.com>
2024-07-12 16:30:16 -0500

    feat

--- funciones.py
+++ funciones.py
@@ -1,3 +1,5 @@
+from datos import *
+
 def actualizar_ciudad(datos):
     datos=dict(datos)
     codigo =input("Ingrese el codigo postal: ")
@@ -36,42 +38,42 @@ def actualizar_ciudad(datos):
             break
     return datos
 
-    def menu_principal():
-    print("--------------------------------------------------")
-    print("Bienvenido a GEOCIUD")
-    print("--------------------------------------------------")
-    print("--------------------------------------------------")
-    print("--------------------------------------------------")
+def menu_principal():
+        print("--------------------------------------------------")
+        print("Bienvenido a GEOCIUD")
+        print("--------------------------------------------------")
+        print("--------------------------------------------------")
+        print("--------------------------------------------------")
 
-    print("¿Que desea realizar?")
-    print("0. Salir de GEOCIUDAD")
-    print("1. agregar ciudad")
-    print("2. editar la informacion de una ciudad")
-    print("3. buscar ciudad")
-    print("--------------------------------------------------")
+        print("¿Que desea realizar?")
+        print("0. Salir de GEOCIUDAD")
+        print("1. agregar ciudad")
+        print("2. editar la informacion de una ciudad")
+        print("3. buscar ciudad")
+        print("--------------------------------------------------")
 
-    while True:
-            
+        while True:
+                
 
-            opcion = str(input("Ingrese la opcion que desea: "))
-            if opcion == "1":
-                print("--------------------------------------------------")
-                print("1") 
-            elif opcion == "2":
-                print("--------------------------------------------------")
-                print("2")   
-            elif opcion == "3":
-                print("--------------------------------------------------")
-                print("3")
-            elif opcion != int:
-                print("--------------------------------------------------")
-                print("opcion invalida")
-            elif opcion == "0":
-                print("--------------------------------------------------")
-                print("Saliendo")
-                 
+                opcion = str(input("Ingrese la opcion que desea: "))
+                if opcion == "1":
+                    print("--------------------------------------------------")
+                    print("1") 
+                elif opcion == "2":
+                    print("--------------------------------------------------")
+                    actualizar_ciudad(datos)
+                elif opcion == "3":
+                    print("--------------------------------------------------")
+                    print("3")
+                elif opcion != int:
+                    print("--------------------------------------------------")
+                    print("opcion invalida")
+                elif opcion == "0":
+                    print("--------------------------------------------------")
+                    print("Saliendo")
+                    
 
-                print("Gracias por usar GEOCIUD")
-                break
+                    print("Gracias por usar GEOCIUD")
+                    break
 
 print(menu_principal())
